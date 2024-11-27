39990204587cb04df5ec94bfd7d987c7704a54e1
developervalclass@gmail.com <developervalclass@gmail.com>
2024-11-27 15:56:41 +0700

    feat: responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
       noFooter
     >
-      <main>
+      <main className='ooverflow-x-hidden w-screen'>
 
 
         <div id='asteroid1b'></div>
@@ -20,42 +20,43 @@ export default function Home() {
         <div id='asteroid3b'></div>
         <div id='asteroid4b'></div>
         <div id='asteroid5b'></div>
+
         <div className='relative w-full h-full flex justify-center'>
-        <div className="absolute w-full h-full overflow-hidden">
-          <div className="w-screen h-[1px] absolute left-0 top-[10%] bg-slate-100"></div>
-          <div className="w-screen h-[1px] absolute left-0 top-[20%] bg-slate-100"></div>
-          <div className="w-screen h-[1px] absolute left-0 top-[30%] bg-slate-100"></div>
-          <div className="w-screen h-[1px] absolute left-0 top-[40%] bg-slate-100"></div>
-          <div className="w-screen h-[1px] absolute left-0 top-[50%] bg-slate-100"></div>
-          <div className="w-screen h-[1px] absolute left-0 top-[60%] bg-slate-100"></div>
-          <div className="w-screen h-[1px] absolute left-0 top-[70%] bg-slate-100"></div>
-          <div className="w-screen h-[1px] absolute left-0 top-[80%] bg-slate-100"></div>
-          <div className="w-screen h-[1px] absolute left-0 top-[90%] bg-slate-100"></div>
-          <div className="w-screen h-[1px] absolute left-0 top-[100%] bg-slate-100"></div>
-        
-          <div className="w-[1px] h-screen absolute top-0 left-[10%] bg-slate-100"></div>
-          <div className="w-[1px] h-screen absolute top-0 left-[20%] bg-slate-100"></div>
-          <div className="w-[1px] h-screen absolute top-0 left-[30%] bg-slate-100"></div>
-          <div className="w-[1px] h-screen absolute top-0 left-[40%] bg-slate-100"></div>
-          <div className="w-[1px] h-screen absolute top-0 left-[50%] bg-slate-100"></div>
-          <div className="w-[1px] h-screen absolute top-0 left-[60%] bg-slate-100"></div>
-          <div className="w-[1px] h-screen absolute top-0 left-[70%] bg-slate-100"></div>
-          <div className="w-[1px] h-screen absolute top-0 left-[80%] bg-slate-100"></div>
-          <div className="w-[1px] h-screen absolute top-0 left-[90%] bg-slate-100"></div>
-          <div className="w-[1px] h-screen absolute top-0 left-[100%] bg-slate-100"></div>
-        </div>
+          <div className="absolute w-full h-full overflow-hidden">
+            <div className="w-screen h-[1px] absolute left-0 top-[10%] bg-slate-100"></div>
+            <div className="w-screen h-[1px] absolute left-0 top-[20%] bg-slate-100"></div>
+            <div className="w-screen h-[1px] absolute left-0 top-[30%] bg-slate-100"></div>
+            <div className="w-screen h-[1px] absolute left-0 top-[40%] bg-slate-100"></div>
+            <div className="w-screen h-[1px] absolute left-0 top-[50%] bg-slate-100"></div>
+            <div className="w-screen h-[1px] absolute left-0 top-[60%] bg-slate-100"></div>
+            <div className="w-screen h-[1px] absolute left-0 top-[70%] bg-slate-100"></div>
+            <div className="w-screen h-[1px] absolute left-0 top-[80%] bg-slate-100"></div>
+            <div className="w-screen h-[1px] absolute left-0 top-[90%] bg-slate-100"></div>
+            <div className="w-screen h-[1px] absolute left-0 top-[100%] bg-slate-100"></div>
+          
+            <div className="w-[1px] h-screen absolute top-0 left-[10%] bg-slate-100"></div>
+            <div className="w-[1px] h-screen absolute top-0 left-[20%] bg-slate-100"></div>
+            <div className="w-[1px] h-screen absolute top-0 left-[30%] bg-slate-100"></div>
+            <div className="w-[1px] h-screen absolute top-0 left-[40%] bg-slate-100"></div>
+            <div className="w-[1px] h-screen absolute top-0 left-[50%] bg-slate-100"></div>
+            <div className="w-[1px] h-screen absolute top-0 left-[60%] bg-slate-100"></div>
+            <div className="w-[1px] h-screen absolute top-0 left-[70%] bg-slate-100"></div>
+            <div className="w-[1px] h-screen absolute top-0 left-[80%] bg-slate-100"></div>
+            <div className="w-[1px] h-screen absolute top-0 left-[90%] bg-slate-100"></div>
+            <div className="w-[1px] h-screen absolute top-0 left-[100%] bg-slate-100"></div>
+          </div>
           <img src='/img/gr.svg' alt='gradient' id='gr1' className='fixed left-[-7%] top-[32%] w-[65%] opacity-60' />
 
           <img src='/img/gr4.svg' alt='gradient1' className='gr1' />
           <img src='/img/gr2.svg' alt='gradient2' className='gr2' />
 
-          <div className='relative w-full lg:mt-0 mt-10 h-max lg:h-[80vh] text-center mx-auto flex flex-col justify-center items-center'>
+          <div className='relative w-full lg:mt-0 mt-9 h-max lg:h-[80vh] text-center mx-auto flex flex-col justify-center items-center'>
             <div className='rounded-full flex bg-blue-100 text-blue-400 h-max w-max px-3 py-1 mb-2 items-center justify-center'>
               <AiMagicIcon className='mr-1' />
               Version 1.0.0 - new released 
               <AiMagicIcon className='ml-1' />
             </div>
-            <h1 className='text-[2.6rem] lg:text-[3.7rem] font-bold w-[90vw] lg:w-[80vw] text-center'>
+            <h1 className='text-[2.5rem] lg:text-[3.7rem] font-bold w-[90vw] lg:w-[80vw] text-center'>
               Build modern website easily <br /> no CSS required.
             </h1>
             <p className='text-slate-500 lg:text-[16px] text-[13px] w-[90%] lg:w-[50%]'>A utility-first CSS framework packed with classes like v-pt-4, <span className='bg-blue-400 text-white rounded-full h-max pb-[2px] text-[14px] px-2'>v-d-flex</span>, text-center and rotate-90 that can be composed to build any design, directly in your code.</p>
@@ -94,7 +95,7 @@ export default function Home() {
               building a small website or a complex web application
             </p>
           </div>
-          <div className='w-full lg:w-[1px] bg-slate-300 h-[1px] lg:h-[200px] lg:inline hidden lg:mx-3'></div>
+          <div className='w-full lg:w-[1px] bg-slate-300 h-[1px] lg:h-[200px] lg:mb-0 mb-3 lg:mx-3'></div>
           <div className='w-full lg:w-1/2 h-max p-3'>
             <h3 className='font-bold mx-auto text-center w-full text-[1.6rem] text-black'>flexiblezel</h3>
             <p className='text-center leading-loose text-slate-500 text-[13px] lg:text-[12px] mt-2'>
